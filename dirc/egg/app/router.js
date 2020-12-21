b8bf0894eea7d012d61806546af12e1f37e60d5c
e <98716067k>
2020-12-21 14:27:42 +0800

    配置了路由

--- dirc/egg/app/router.js
+++ dirc/egg/app/router.js
@@ -5,6 +5,6 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.post("/register",controller.home.register);
+  router.post("//UserChange",controller.home.register);
   router.get("/verif",controller.home.verif);
 };
